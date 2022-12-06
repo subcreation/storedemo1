e08edee15b671b41e2f6bad2b9d131bec3ab56bc
Nathanael Roberton <in8media@gmail.com>
2022-12-06 05:01:34 -0800

    Initial Commit

---
+++ StoreDemo1/StoreDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  StoreDemo1App.swift
+//  StoreDemo1
+//
+//  Created by Nathanael Roberton on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct StoreDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
